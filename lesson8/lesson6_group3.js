9490744ea3e08fdee04e3fa862a33c513ae98560
Denis Yermolin <34105970+Vivatool@users.noreply.github.com>
2024-10-24 21:00:11 +0300

    Add files via upload

---
+++ lesson8/lesson6_group3.js
@@ -0,0 +1,25 @@
+let str = "String";
+let letter = str[1]; 
+console.log(letter);
+
+
+let text = "0";
+let length = text.length;
+console.log(length);
+
+
+var result = "js"/0;
+console.log(nan)
+
+
+var guessWho = 10.27;
+guessWho = 'Here is sentence';
+guessWho = true;
+guessWho = 8.28;
+console.log(guessWho)
+
+
+const num = 20;
+const str1 = "Plov";
+const result = num + str1; 
+console.log(result)
---
+++ lesson8/lesson6_group4.js
@@ -0,0 +1,25 @@
+let str = "Arrays";
+let letter = str[1]; 
+console.log(letter);
+
+
+let text = "";
+let length = text.length;
+console.log(length);
+
+
+var result = "string"/0;
+console.log(nan)
+
+
+var guessWho = 2.84;
+guessWho = 'А тепер тут рядок';
+guessWho = false;
+guessWho = 2.32;
+console.log(guessWho)
+
+
+const num = 24;
+const str1 = "Golubci";
+const result = num + str1; 
+console.log(result)
